e52cba3ca8c1592a9fbbcc62731586aa203f028e
Jarred Capellman <jcapellman@hotmail.com>
2022-08-21 12:20:26 -0500

    Added: Stub for Users

---
+++ jcGAI/jcGAI.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using jcGAI.WebAPI.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcGAI.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        private readonly MongoDBService _mongo;
+
+        public UsersController(ILogger<UsersController> logger, MongoDBService mongo)
+        {
+            _logger = logger;
+            _mongo = mongo;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateUserAsync(string username, string password)
+        {
+            return Guid.NewGuid(); 
+        }
+    }
+}
