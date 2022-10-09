d8f421d8edf0e3fc9828b0fc940112824b65e63b
Jarred Capellman <jcapellman@hotmail.com>
2022-10-09 16:17:22 -0500

    Added: Default Constructor Test for the InsightsManager

---
+++ jcGAI.UnitTests/Managers/InsightsManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using jcGAI.WebAPI.Managers;
+using jcGAI.WebAPI.Objects.Config;
+
+namespace jcGAI.UnitTests.Managers
+{
+    [TestClass]
+    public class InsightsManagerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsightsManager_DefaultConstructor()
+        {
+            _ = new InsightsManager(new WebAPI.Services.MongoDbService(new MongoDbConfig()));
+        }
+    }
+}
